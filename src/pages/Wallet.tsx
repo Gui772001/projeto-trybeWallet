f3fc918354f7156a63a21eefdb3a799432879612
Guilherme <gulhermegui772001@gmail.com>
2023-12-12 19:58:44 -0300

    ACABEI

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,8 +1,11 @@
+import { useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
 
 function Wallet() {
+  const variavel = useLocation();
+  console.log(variavel);
   return (
     <>
       <Header />
